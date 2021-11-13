0439e7dfb184082b91599d7bfd42e05c19cffc58
Steve Moorhouse <albanycomputers@users.noreply.github.com>
2021-11-13 22:00:10 +0000

    Initial upload

---
+++ README.md
@@ -0,0 +1,66 @@
+Views Grid Repeat
+------------------------
+
+Provides ability to put Views content in responsive columns using CSS3 Grid layout.
+
+INTRODUCTION
+------------
+
+This was created from the existing module Views B Grid
+
+https://backdropcms.org/project/views_b_grid
+
+Allow designers to create a grid content without or without creating their own CSS.
+
+The module creates a new Views Format to display content in a CSS3 Grid.  You can select either Fit or Fill see https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/ 
+
+
+You can set the width of the content and the minimum height of the content to help create a more balanced grid.  The content will automatically wrap based on the width of the region.  No need to use @media queries for different viewport widths.
+
+
+For more information:
+
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+http://flexbox.io/
+
+
+REQUIREMENTS
+------------
+
+Views UI
+
+
+INSTALLATION
+------------
+
+Install this module using the official Backdrop CMS instructions at https://backdropcms.org/guide/modules
+
+
+USAGE
+-----
+
+When editing a View, choose:
+format: CSS3 Grid Fit or Fill
+
+Settings: 
+    Content width: Choose the width of the content
+    Minimum row height: Choose the minimum row height
+    Unit type for width and height: for the Unit or measure i.e.  px, em, ren, cm, mm, in, vh etc...
+    See https://www.w3schools.com/cssref/css_units.asp for all of the available options.
+
+LICENSE
+-------
+
+This project is GPL v2 software. See the LICENSE.txt file in this directory for complete text.
+
+CREDITS
+-----------
+
+ - Based on views_b4_full by biolithic <https://github.com/biolithic>
+ and
+ - Based on Views B Grid by herbdool https://github.com/herbdool
+
+MAINTAINERS
+-----------
+
+ - albanycomputers <https://github.com/albanycomputers>
---
+++ views-view-views-grid-repeat.tpl.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file
+ * Default view template to display content using views_grid_repeat layout_type
+ */
+
+$layout_type = $options['layout_type']; // fit or fill the grid
+$layout_width_value = $options['layout_width_value'];  // width of the column or content
+$layout_width_height = $options['layout_width_height']; // minimum height of content
+$layout_width_unit = trim($options['layout_width_unit']); // unit of measire for the content value i.e. px or em etc...
+
+$css_style_pre = '';
+$css_style = 'style="';
+$css_style_post = '"';
+
+$css_width_value = '';
+if ($layout_width_value>0) {
+  $css_width_value = strval($layout_width_value) . $layout_width_unit ;
+  $css_style .= 'display:grid;grid-template-columns:repeat(auto-fit, minmax(' . $css_width_value . ', 1fr));';
+}
+
+$css_width_height = '';
+if($layout_width_height>0) {
+  $css_width_height = strval($layout_width_height) . $layout_width_unit ;
+  $css_style .= ' min-height:' . $css_width_height . ';' . $css_style_post;
+}
+
+
+
+switch ($layout_type) {
+  
+    case "fit_grid":
+        print '<div class="grid-repeat-container-fit" ' . $css_style . '>';
+          //print '<div class="grid-template-columns:repeat(auto-fit, minmax(250px, 1fr))">';
+            //print '<div class="row">';
+            if (!empty($title)) {
+                print "<h3>$title</h3>";
+            }
+            foreach ($rows as $id => $row) {
+                if ($row_classes[$id]) {
+                    print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
+                }
+            }
+            //print '</div>';
+          //print '</div>';
+        print '</div>';
+    break;
+
+    case "fill_grid":
+      print '<div class="grid-repeat-container-fill" ' . $css_style . '>';
+          if (!empty($title)) {
+              print "<h3>$title</h3>";
+          }
+          foreach ($rows as $id => $row) {
+              if ($row_classes[$id]) {
+                print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
+              }
+          }
+      print '</div>';
+  break;
+}
+
+?>
