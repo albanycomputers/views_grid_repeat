837cac0346a6e3920f59f16573e28d2e9b1ac1da
Steve Moorhousek <albanycomputers@users.noreply.github.com>
2021-11-14 11:25:48 +0000

    Bug fix and coding standard fixes
    Fixed bug where only FIT was being added to the css style

--- views-view-views-grid-repeat.tpl.php
+++ views-view-views-grid-repeat.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Default view template to display content using views_grid_repeat layout_type
+ * Default view template to display content using views_grid_repeat layout_type.
  */
 
 // Fit or fill the grid.
@@ -19,44 +19,48 @@ $layout_width_unit = trim($options['layout_width_unit']);
 
 $css_style = '';
 $css_style_post = '"';
+$css_syle_fit = 'auto-fit';
+$css_syle_fit = 'auto-fill';
 
 $css_width_value = '';
 if ($layout_width_value > 0) {
-  $css_width_value = strval($layout_width_value) . $layout_width_unit ;
-  $css_style = 'style="display:grid;grid-template-columns:repeat(auto-fit, minmax(' . $css_width_value . ', 1fr));';
+  $css_width_value = strval($layout_width_value) . $layout_width_unit;
+  $css_style = 'style="display:grid;grid-template-columns:repeat(auto----, minmax(' . $css_width_value . ', 1fr));';
 
   $css_width_height = '';
   if ($layout_width_height > 0) {
-    $css_width_height = strval($layout_width_height) . $layout_width_unit ;
+    $css_width_height = strval($layout_width_height) . $layout_width_unit;
     $css_style .= ' min-height:' . $css_width_height . ';' . $css_style_post;
   }
 }
 
 switch ($layout_type) {
   
-    case "fit_grid":
-        print '<div class="grid-repeat-container-fit" ' . $css_style . '>';
-            if (!empty($title)) {
-                print "<h3>$title</h3>";
-            }
-            foreach ($rows as $id => $row) {
-                if ($row_classes[$id]) {
-                    print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
-                }
-            }
-        print '</div>';
-    break;
+  case "fit_grid":
+    $css_style = str_replace('auto----','auto-fit',$css_style);        
+    print '<div class="grid-repeat-container-fit" ' . $css_style . '>';
+    if (!empty($title)) {
+      print "<h3>$title</h3>";
+    }
+    foreach ($rows as $id => $row) {
+      if ($row_classes[$id]) {
+        print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
+    }
+  }
+  print '</div>';
+  break;
 
-    case "fill_grid":
-      print '<div class="grid-repeat-container-fill" ' . $css_style . '>';
-          if (!empty($title)) {
-              print "<h3>$title</h3>";
-          }
-          foreach ($rows as $id => $row) {
-              if ($row_classes[$id]) {
-                print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
-              }
-          }
-      print '</div>';
+  case "fill_grid":
+    $css_style = str_replace("auto----","auto-fill",$css_style);
+    print '<div class="grid-repeat-container-fill" ' . $css_style . '>';
+    if (!empty($title)) {
+      print "<h3>$title</h3>";
+    }
+    foreach ($rows as $id => $row) {
+      if ($row_classes[$id]) {
+        print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
+      }
+    }
+    print '</div>';
   break;
 }
