8b9ee8f34c96f278cea6e9300f4199410729abc5
Steve Moorhousek <albanycomputers@users.noreply.github.com>
2021-11-24 21:12:01 +0000

    Changed logic and reduced code.
    Added option to use either Inline CSS or Theme CSS.  TODO: javascript to disable options if Theme CSS is selected.

--- views-view-views-grid-repeat.tpl.php
+++ views-view-views-grid-repeat.tpl.php
@@ -6,61 +6,67 @@
  */
 
 // Fit or fill the grid.
-$layout_type = $options['layout_type'];
+$layout_inline = $options['layout_inline'];
 
-// Width of the column or content.
-$layout_width_value = $options['layout_width_value'];
+if ($layout_inline == 'inline_css') {
+  // either 'fit_grid' or 'fill_frid'.
+  $layout_type = $options['layout_type'];
 
-// Minimum height of content.
-$layout_width_height = $options['layout_width_height'];
+  // Width of the column or content.
+  $layout_width_value = $options['layout_width_value'];
 
-// Unit of measire for the content value i.e. px or em etc.
-$layout_width_unit = trim($options['layout_width_unit']);
+  // Minimum height of content.
+  $layout_width_height = $options['layout_width_height'];
 
-$css_style = '';
-$css_style_post = '"';
-$css_syle_fit = 'auto-fit';
-$css_syle_fit = 'auto-fill';
+  // Unit of measure for the content value i.e. px or em etc.
+  $layout_width_unit = trim($options['layout_width_unit']);
 
-$css_width_value = '';
-if ($layout_width_value > 0) {
+  // Pre and Post CSS.
+  $css_width_value = '';
   $css_width_value = strval($layout_width_value) . $layout_width_unit;
-  $css_style = 'style="display:grid;grid-template-columns:repeat(auto----, minmax(' . $css_width_value . ', 1fr));';
-
-  $css_width_height = '';
-  if ($layout_width_height > 0) {
-    $css_width_height = strval($layout_width_height) . $layout_width_unit;
-    $css_style .= ' min-height:' . $css_width_height . ';' . $css_style_post;
-  }
-}
-
-switch ($layout_type) {
   
-  case "fit_grid":
-    $css_style = str_replace('auto----','auto-fit',$css_style);        
-    print '<div class="grid-repeat-container-fit" ' . $css_style . '>';
-    if (!empty($title)) {
-      print "<h3>$title</h3>";
-    }
-    foreach ($rows as $id => $row) {
-      if ($row_classes[$id]) {
-        print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
+  $css_width_height = '';
+  $css_width_height = strval($layout_width_height) . $layout_width_unit;
+
+  $css_style = '';
+  $css_style = 'style="display:grid;grid-template-columns:repeat(auto----, minmax(' . $css_width_value . ', 1fr)); min-height:' . $css_width_height . ';"';
+
+  // The four extra dashes after 'container' below are a holding place for string replacement.
+  $div_class = '';
+  $div_class = '<div class="grid-repeat-container-----" ';
+
+  switch ($layout_type) {
+    case "fit_grid":
+      $css_style = str_replace('auto----','auto-fit',$css_style);
+      $div_class = str_replace('-----','-fit',$div_class);
+    break;
+
+    case "fill_grid":
+      $css_style = str_replace("auto----","auto-fill",$css_style);
+      $div_class = str_replace('-----','-fill',$div_class);
+    break;
+  }
+
+  print $div_class . $css_style . '>';
+  if (!empty($title)) {
+    print "<h3>$title</h3>";
+  }
+  foreach ($rows as $id => $row) {
+    if ($row_classes[$id]) {
+      print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
     }
   }
   print '</div>';
-  break;
-
-  case "fill_grid":
-    $css_style = str_replace("auto----","auto-fill",$css_style);
-    print '<div class="grid-repeat-container-fill" ' . $css_style . '>';
-    if (!empty($title)) {
-      print "<h3>$title</h3>";
+}
+else
+{
+  print '<div class="views-grid-repeat">';
+  if (!empty($title)) {
+    print "<h3>$title</h3>";
+  }
+  foreach ($rows as $id => $row) {
+    if ($row_classes[$id]) {
+      print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
     }
-    foreach ($rows as $id => $row) {
-      if ($row_classes[$id]) {
-        print '<div class="' . implode(' ', $row_classes[$id]) . '">' . $row . '</div>';
-      }
-    }
-    print '</div>';
-  break;
+  }
 }
